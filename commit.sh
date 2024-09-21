2ff104550387b5d5f381c87b722dd2b18c300e8f
Hoa Huu Nguyen <hoaboy89@gmail.com>
2024-09-22 03:03:39 +0700

    2024-09-22 03:03:39: add init file

--- commit.sh
+++ commit.sh
@@ -7,12 +7,12 @@ then
   exit 1
 fi
 
-# Kiểm tra xem người dùng có truyền vào tên nhánh hay không
-if [ -z "$2" ]
-then
-  echo "please enter the name of the branch."
-  exit 1
-fi
+# # Kiểm tra xem người dùng có truyền vào tên nhánh hay không
+# if [ -z "$2" ]
+# then
+#   echo "please enter the name of the branch."
+#   exit 1
+# fi
 
 # Lấy ngày giờ hiện tại
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
@@ -23,4 +23,4 @@ commit_message="$current_date: $1"
 # Thực hiện các lệnh git
 git add .
 git commit -m "$commit_message"
-git push origin "$2"
+git push
